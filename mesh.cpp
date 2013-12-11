4e1baf3fe8b58a937258d9b6faf8814dfb3fc0e9
Philipp Moritz <pcmoritz@gmail.com>
2013-12-11 04:22:13 -0800

    meshing prototype done

--- mesh.cpp
+++ mesh.cpp
@@ -20,106 +20,114 @@
 #include <dolfin/mesh/MeshEditor.h>
 #include <dolfin/mesh/MeshPartitioning.h>
 
+#include <dolfin/plot/plot.h>
+
 #define HAS_CGAL
 
 #include <dolfin/generation/CGALMeshBuilder.h>
-#include "cgal_triangulate_polyhedron.h"
-// #include "cgal_csg3d.h"
 
-#include <dolfin/plot/plot.h>
+#include "cgal_triangulate_polyhedron.h"
+#include "cgal_copy_polygon_to.h"
+#include "cgal_csg3d.h"
 
 using namespace dolfin;
 
-typedef CGAL::Exact_predicates_exact_constructions_kernel Exact_Kernel;
-typedef CGAL::Nef_polyhedron_3<Exact_Kernel> Nef_polyhedron_3;
-    typedef CGAL::Polyhedron_3<Exact_Kernel> Exact_Polyhedron_3;
-
-typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
-
-typedef CGAL::Polyhedral_mesh_domain_with_features_3<K> Mesh_domain;
-
-typedef CGAL::Robust_weighted_circumcenter_filtered_traits_3<K> Geom_traits;
-
-// CGAL 3D triangulation vertex typedefs
-typedef CGAL::Triangulation_vertex_base_3<Geom_traits> Tvb3test_base;
-typedef CGAL::Triangulation_vertex_base_with_info_3<int, Geom_traits, Tvb3test_base> Tvb3test;
-typedef CGAL::Mesh_vertex_base_3<Geom_traits, Mesh_domain, Tvb3test> Vertex_base;
-
-// CGAL 3D triangulation cell typedefs
-typedef CGAL::Triangulation_cell_base_3<Geom_traits> Tcb3test_base;
-typedef CGAL::Triangulation_cell_base_with_info_3<int, Geom_traits, Tcb3test_base> Tcb3test;
-typedef CGAL::Mesh_cell_base_3<Geom_traits, Mesh_domain, Tcb3test> Cell_base;
-
-// CGAL 3D triangulation typedefs
-typedef CGAL::Triangulation_data_structure_3<Vertex_base, Cell_base> Tds_mesh;
-typedef CGAL::Regular_triangulation_3<Geom_traits, Tds_mesh> Tr;
-
-// CGAL 3D mesh typedef
-typedef CGAL::Mesh_complex_3_in_triangulation_3<
-  Tr, Mesh_domain::Corner_index, Mesh_domain::Curve_segment_index> C3t3;
-
-// Mesh criteria
-typedef CGAL::Mesh_criteria_3<Tr> Mesh_criteria;
-
-// Typedefs for building CGAL polyhedron from list of facets
-typedef CGAL::Mesh_polyhedron_3<K>::Type Polyhedron;
-typedef Polyhedron::Facet_iterator Facet_iterator;
-typedef Polyhedron::Halfedge_around_facet_circulator Halfedge_facet_circulator;
-typedef Polyhedron::HalfedgeDS HalfedgeDS;
-
-template<typename T>
-void cgal_generate(Mesh& mesh, T& p, double cell_size,
-		   bool detect_sharp_features)
+void build_mesh(const csg::C3t3& c3t3, Mesh& mesh)
 {
-  // Check if any facets are not triangular and triangulate if necessary.
-  // The CGAL mesh generation only supports polyhedra with triangular surface
-  // facets.
+  typedef csg::C3t3 C3T3;
+  typedef C3T3::Triangulation Triangulation;
+  typedef Triangulation::Vertex_handle Vertex_handle;
 
-  typename Polyhedron::Facet_iterator facet;
-  for (facet = p.facets_begin(); facet != p.facets_end(); ++facet)
+  // CGAL triangulation
+  const Triangulation& triangulation = c3t3.triangulation();
+
+  // Clear mesh
+  mesh.clear();
+
+  // Count cells in complex
+  std::size_t num_cells = 0;
+  for(csg::C3t3::Cells_in_complex_iterator cit = c3t3.cells_in_complex_begin();
+      cit != c3t3.cells_in_complex_end();
+      ++cit)
   {
-    // Check if there is a non-triangular facet
-    if (!facet->is_triangle())
-    {
-      CGAL::triangulate_polyhedron(p);
-      break;
-    }
+    num_cells++;
   }
 
-  // Create domain from polyhedron
-  Mesh_domain domain(p);
+  // Create and initialize mesh editor
+  dolfin::MeshEditor mesh_editor;
+  mesh_editor.open(mesh, 3, 3);
+  mesh_editor.init_vertices(triangulation.number_of_vertices());
+  mesh_editor.init_cells(num_cells);
 
-  // Get sharp features
-  if (detect_sharp_features)
-    domain.detect_features();
+  // Add vertices to mesh
+  std::size_t vertex_index = 0;
+  std::map<Vertex_handle, std::size_t> vertex_id_map;
 
-  const Mesh_criteria criteria(CGAL::parameters::facet_angle = 25,
-                               CGAL::parameters::facet_size = cell_size,
-                               CGAL::parameters::cell_radius_edge_ratio = 3.0,
-                               CGAL::parameters::edge_size = cell_size);
+  for (Triangulation::Finite_vertices_iterator
+         cgal_vertex = triangulation.finite_vertices_begin();
+       cgal_vertex != triangulation.finite_vertices_end(); ++cgal_vertex)
+  {
+    vertex_id_map[cgal_vertex] = vertex_index;
 
-  // Generate CGAL mesh
-  C3t3 c3t3 = CGAL::make_mesh_3<C3t3>(domain, criteria);
+      // Get vertex coordinates and add vertex to the mesh
+    Point p(cgal_vertex->point()[0], cgal_vertex->point()[1], cgal_vertex->point()[2]);
+    mesh_editor.add_vertex(vertex_index, p);
 
-  // Build DOLFIN mesh from CGAL mesh/triangulation
-  CGALMeshBuilder::build_from_mesh(mesh, c3t3);
+    ++vertex_index;
+  }
+
+  // Add cells to mesh
+  std::size_t cell_index = 0;
+  for(csg::C3t3::Cells_in_complex_iterator cit = c3t3.cells_in_complex_begin();
+      cit != c3t3.cells_in_complex_end();
+      ++cit)
+  {
+    mesh_editor.add_cell(cell_index,
+                         vertex_id_map[cit->vertex(0)],
+                         vertex_id_map[cit->vertex(1)],
+                         vertex_id_map[cit->vertex(2)],
+                         vertex_id_map[cit->vertex(3)]);
+
+    ++cell_index;
+  }
+
+  // Close mesh editor
+  mesh_editor.close();
 }
+
+void generate(Mesh& mesh, const csg::Polyhedron_3& p, double cell_size) {
+  dolfin_assert(p.is_pure_triangle());
+  csg::Mesh_domain domain(p);
+  domain.detect_features();
+  csg::Mesh_criteria criteria(CGAL::parameters::facet_angle = 25,
+			      CGAL::parameters::facet_size = cell_size,
+			      CGAL::parameters::cell_radius_edge_ratio = 3.0,
+			      CGAL::parameters::edge_size = cell_size);
+  csg::C3t3 c3t3 = CGAL::make_mesh_3<csg::C3t3>(domain, criteria,
+                                                CGAL::parameters::no_perturb(),
+                                                CGAL::parameters::no_exude());
+  build_mesh(c3t3, mesh);
+}
+
+
 
 // Apply a transformation of the form QP + t to the polyhedron P where
 // Q is the rotation given by a quaternion (x, y, z, w) and t is a
 // translation vector.
-Polyhedron transformed_polyhedron(Polyhedron& P, double x, double y,
-				  double z, double w, double tx, double ty,
-				  double tz) {
+csg::Exact_Polyhedron_3
+transformed(csg::Exact_Polyhedron_3& P,
+	    double x, double y, double z,
+	    double w, double tx, double ty, double tz) {
   double s = std::sin(w);
   double c = std::cos(w);
   // Rotation
-  CGAL::Aff_transformation_3<K>
+  CGAL::Aff_transformation_3<csg::Exact_Kernel>
     R(c + x*x*(1-c), x*y*(1-c) - z*s, x*z*(1-c) + y*s,
       y*x*(1-c) + z*s, c + y*y*(1-c), y*z*(1-c) - x*s,
       z*x*(1-c) - y*x, z*y*(1-c) + x*s, c + z*z*(1-c));
   // Translation
-  CGAL::Aff_transformation_3<K> T(1, 0, 0, tx, 0, 1, 0, ty, 0, 0, 1, tz);
+  CGAL::Aff_transformation_3<csg::Exact_Kernel>
+    T(1, 0, 0, tx, 0, 1, 0, ty, 0, 0, 1, tz);
   std::transform(P.points_begin(), P.points_end(), P.points_begin(), T*R);
   return P;
 }
@@ -127,29 +135,37 @@ Polyhedron transformed_polyhedron(Polyhedron& P, double x, double y,
 
 int main() {
   std::string off_file = "../cube.off";
-  Polyhedron p; // unit cube
+  csg::Exact_Polyhedron_3 cube; // unit cube
   std::cout << "reading file " << off_file << std::endl;
-  std::ifstream p_file(off_file.c_str());
-  p_file >> p;
+  std::ifstream file(off_file.c_str());
+  file >> cube;
   std::cout << "done reading file." << std::endl;
   double cell_size = 0.5;
   bool detect_sharp_features = true;
   Mesh mesh;
   
-  Polyhedron outer(p);
+  csg::Exact_Polyhedron_3 outer(cube);
   
   // scale and translate the outer box
-  CGAL::Aff_transformation_3<K> S(4, 0, 0, -1, 0, 3, 0, -1, 0, 0, 2.5, -1);
+  CGAL::Aff_transformation_3<csg::Exact_Kernel> S(4, 0, 0, -1, 0, 3, 0, -1, 0, 0, 2.5, -1);
   std::transform(outer.points_begin(), outer.points_end(), 
-		 outer.points_begin(), S);
+  		 outer.points_begin(), S);
 
-  Polyhedron first_inner(p);
+  csg::Nef_polyhedron_3 Omega(outer);
+  csg::Exact_Polyhedron_3 first_inner(cube);
+  csg::Exact_Polyhedron_3 second_inner(cube);
+  second_inner = transformed(second_inner, 0, 0, 1, 0.1, 1.5, 0, 0);
+  
+  Omega -= first_inner;
+  Omega -= second_inner;
 
-  Nef_polyhedron_3 Omega();
+  csg::Exact_Polyhedron_3 p;
+  Omega.convert_to_polyhedron(p);
+  csg::Polyhedron_3 q;
+  copy_to(p, q);
+  
 
-  // Omega -= first_inner;
-
-  cgal_generate(mesh, p, cell_size, detect_sharp_features);
+  generate(mesh, q, cell_size);
   plot(mesh, "mesh of a cube");
   interactive(true);
   std::getchar();

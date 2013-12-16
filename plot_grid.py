79c600a5566e420c387598d45ab6490ee96fe043
Philipp Moritz <pcmoritz@gmail.com>
2013-12-15 22:06:31 -0800

    get drawing to work

--- plot_grid.py
+++ plot_grid.py
@@ -4,10 +4,11 @@ import networkx as nx
 # The grid we draw is N by N by N, each field is w wide
 N = 5
 w = 0.1
-input_file = open('~/function-values.txt', 'r')
+input_file = open('function-values.txt', 'r')
 
 # read in the file
-content = input_file.readlines()
+
+content = [line.strip() for line in input_file]
 
 def index(x, y, z):
     return x * N * N + y * N + z
@@ -25,26 +26,31 @@ S = numpy.ones(N * N * N)
 G = nx.Graph()
 
 # set nodes appropriately for the "unit map" grid
-# for x in range(N):
-#     for y in range(N):
-#         for z in range(N):
-#             X[index(x, y, z)] = x * w
-#             Y[index(x, y, z)] = y * w
-#             Z[index(x, y, z)] = z * w
+for x in range(N):
+    for y in range(N):
+        for z in range(N):
+            X[index(x, y, z)] = x * w
+            Y[index(x, y, z)] = y * w
+            Z[index(x, y, z)] = z * w
 
-index = 0
+idx = 0
 
 # read in the grid from the file
 for entry in content:
     if entry == "NA":
-        X[index] = float('NaN')
-        Y[index] = float('NaN')
-        Z[index] = float('NaN')
+        X[idx] = float('NaN')
+        Y[idx] = float('NaN')
+        Z[idx] = float('NaN')
     else:
-        X[index] = entry[0]
-        X[index] = entry[1]
-        X[index] = entry[2]
-    index = index + 1
+        floats = [float(x) for x in entry.split()]
+        X[idx] = floats[0]/8
+        Y[idx] = floats[1]/6
+        Z[idx] = floats[2]/4
+    idx = idx + 1
+
+print X
+print Y
+print Z
 
 # connect the graph
 for x in range(N):
@@ -53,20 +59,25 @@ for x in range(N):
             if X[index(x, y, z)] == float('NaN'):
                 continue
             
-            if not x == N-1 and not X[index(x+1, y, z)] != float('NaN'):
+            if not x == N-1 and X[index(x+1, y, z)] != float('NaN'):
                 G.add_edge(index(x, y, z), index(x+1, y, z))
-            if not y == N-1 and not X[index(x, y+1, z)] != float('NaN'):
+            if not y == N-1 and X[index(x, y+1, z)] != float('NaN'):
                 G.add_edge(index(x, y, z), index(x, y+1, z))
-            if not z == N-1 and not X[index(x, y, z+1)] != float('NaN'):
+            if not z == N-1 and X[index(x, y, z+1)] != float('NaN'):
                 G.add_edge(index(x, y, z), index(x, y, z+1))
 
 node_size=0.005
 edge_color=(0.8, 0.8, 0.8)
 edge_size=0.001
 
-nodes = sorted(G.nodes())
+# nodes = sorted(G.nodes())
             
-pts = mlab.points3d(X[nodes], Y[nodes], Z[nodes], S[nodes], 
+# pts = mlab.points3d(X[nodes], Y[nodes], Z[nodes], S[nodes], 
+#                    scale_factor=node_size)
+
+print G.edges()
+
+pts = mlab.points3d(X, Y, Z, S, 
                     scale_factor=node_size)
 pts.mlab_source.dataset.lines = numpy.array(G.edges())
 tube = mlab.pipeline.tube(pts, tube_radius=edge_size)
